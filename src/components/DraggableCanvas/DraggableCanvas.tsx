d95fa4632e905c234581cd1e831c3cd8da47f43b
Alan Oliveira <alan@mykargo.com>
2022-11-15 19:46:37 -0300

    feat(draw): basic canvas drawing function

--- src/components/DraggableCanvas/DraggableCanvas.tsx
+++ src/components/DraggableCanvas/DraggableCanvas.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 type CanvasContainerProps = {
@@ -18,6 +18,30 @@ type DraggableCanvasProps = {
 
 const DraggableCanvas = ({ width, height }: DraggableCanvasProps) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const images = [{ color: 'blue' }, { color: 'red' }, { color: 'green' }];
+
+  const draw = useCallback(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+
+    const ctx = canvas.getContext('2d');
+    if (!ctx) {
+      return;
+    }
+
+    images.forEach((image, index) => {
+      ctx.fillStyle = image.color;
+      ctx.fillRect(index * width, 0, width, height);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (canvasRef) {
+      draw();
+    }
+  }, [canvasRef]);
 
   return (
     <CanvasContainer width={width} height={height}>
